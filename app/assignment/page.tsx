8ded8d92c2cb3c2f295acc24078fa96a6effb94c
gerynsb <118332285+gerynsb@users.noreply.github.com>
2024-12-25 17:04:08 +0800

    build : header di courses dan assignment page

--- app/assignment/page.tsx
+++ app/assignment/page.tsx
@@ -1,7 +1,19 @@
+"use client"
+
+import Header from "@/components/Header";
+
+
+
 const AssignmentPage = () => {
     return (
-      <div className="flex flex-col items-center justify-center flex-1 bg-[#F4F6FA] text-black">
-        <h1 className="text-3xl font-bold">Welcome to Assignment Page</h1>
+      <div className="min-h-screen flex bg-[#F4F6FA]">
+        <div className="flex flex-col flex-1">
+          <Header />
+
+          <div className="flex flex-col flex-1 items-center justify-center text-black">
+            <h1 className="text-4xl font-bold">Welcome to Assignment Page</h1>
+          </div>
+        </div>
       </div>
     );
   };
--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,10 +1,23 @@
+"use client";
+
+import Header from "@/components/Header"; // Impor Header
+
 const CoursesPage = () => {
-    return (
-      <div className="flex flex-col items-center justify-center flex-1 bg-[#F4F6FA] text-black">
-        <h1 className="text-3xl font-bold">Welcome to Courses Page</h1>
+  return (
+    <div className="min-h-screen flex bg-[#F4F6FA]">
+
+      {/* Konten Utama */}
+      <div className="flex flex-col flex-1">
+        {/* Header */}
+        <Header />
+
+        {/* Main Content */}
+        <div className="flex flex-1 flex-col items-center justify-center text-black">
+          <h1 className="text-3xl font-bold">Welcome to Courses Page</h1>
+        </div>
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
+
 export default CoursesPage;
-  
